a50e238815ef0e7bdca8ab0ebf80973ea60e0f49
Ehern15 <erikhern15@gmail.com>
2022-11-16 19:46:15 -0500

    added getMax method

--- src/radixSort.java
+++ src/radixSort.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
 class radixSort{
-
-public void performRadixSort(int arr[]){
-
+public static int getMax(int arr[]){
+    int max = arr[0];
+    for(int i = 0; i < arr.length;i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+public static void sortInBucket(int arr[]){
+    int[] bucket = new int[10];
+    for(int i = 0;i < arr.length;i++){
+        
+        arr[i] % 10;
+        
+    }
+    System.out.println(bucket);
 }
 public static void main(String[] args) {
     int[] input1 = {24, 12, 4, 366, 45, 66, 8, 14};
     int[] input2 = {24, 2, 4, 466, 48, 66, 8, 44};
-    System.out.println();
+    sortInBucket(input1);
 
 }
 
