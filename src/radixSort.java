667598b2fc6140a3cf5064490958cb83bff77eaa
Ehern15 <erikhern15@gmail.com>
2022-11-16 20:50:19 -0500

    created a new Branch

--- src/radixSort.java
+++ src/radixSort.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 class radixSort{
 public static int getMax(int arr[]){
     int max = arr[0];
@@ -9,19 +10,26 @@ public static int getMax(int arr[]){
     }
     return max;
 }
+public static void printArray(int arr[]){
+   
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print( arr[i] + " ");
+    }
+   System.out.println();
+}
 public static void countSort(int arr[]){
     int[] bucket = new int[10];
+    System.out.println(arr.length);
     for(int i = 0;i < arr.length;i++){
         
         
         
     }
-    System.out.println(bucket);
 }
 public static void main(String[] args) {
     int[] input1 = {24, 12, 4, 366, 45, 66, 8, 14};
     int[] input2 = {24, 2, 4, 466, 48, 66, 8, 44};
-    
+    printArray(input2);
 
 }
 
