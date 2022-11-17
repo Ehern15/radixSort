141790d5552382cb86cfd85c62a5c3eaf0664315
Ehern15 <erikhern15@gmail.com>
2022-11-17 17:13:17 -0500

    completed countSort method

--- src/radixSort.java
+++ src/radixSort.java
@@ -2,11 +2,13 @@ import java.io.*;
 import java.util.*;
 
 class radixSort{
-public static boolean detectEven(int arr[]){
+public static boolean detectOdd(int arr[]){
     boolean temp = false;
         for (int i = 0; i < arr.length && temp !=true; i++) {
             if(arr[i] % 2 == 0){
-            temp = true;
+            
+            }else{
+                temp = false;
             }
 
         }
@@ -35,35 +37,48 @@ public static void countSort(int arr[], int numsPlace){
         bucket.set(i,0);
     }
     int temp;
+    int[] output;
+    output = new int[arr.length];
+    Arrays.fill(output,0);
 
     for(int i = 0;i < arr.length;i++){
         temp = arr[i] % numsPlace;
         bucket.set(temp,bucket.get(temp) + 1);
         if(i > 0){
-            bucket[i] = bucket[i] + bucket[i-1];
-
+            bucket.set(i, bucket.get(i) + bucket.get(i-1));
         }
     }
-    for (int i = 0; i < bucket.length; i++) {
+    for (int i = 0; i < bucket.size(); i++) {
+        if(bucket.get(i) > 0){
         
+        output[bucket.get((arr[i] / numsPlace) % 10) - 1] = arr[i];
+        bucket.set(i,bucket.get(i)- 1);
+        }
     }
-    printArray(bucket);
+    for (int i = 0; i < output.length; i++) {
+        arr[i] = output[i];
+    }
 }
-public static void performRadixSort(int arr[],int max){
-    
-    for (int numsPlace = 1; max / numsPlace > 0; numsPlace *= 10)
+public static void performRadixSort(int arr[]){
+    int max = getMax(arr);
+    for (int numsPlace = 1; max / numsPlace > 0; numsPlace *= 10){
             countSort(arr, numsPlace);
+        }
+    printArray(arr);
     }
-}
+    // TODO somethings broken here
+    
+
 public static void main(String[] args) {
     int[] input1 = {24, 12, 4, 366, 45, 66, 8, 14};
     int[] input2 = {24, 2, 4, 466, 48, 66, 8, 44};
-    if (detectEven(input1)){
+    if (detectOdd(input1)){
         System.out.println("***Even Detected. Terminating Program.***");
         System.exit(1);
     }else{
-    printArray(input1);
-    countSort(input1);
+        
+        performRadixSort(input1);
+    
     }
 
 }
